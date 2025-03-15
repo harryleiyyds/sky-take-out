16d6bf87d63ed81a36aca86a00e9e8620af53387
harryleiyyds <harryleiyyds@gmail.com>
2025-03-15 20:19:11 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -31,5 +33,12 @@ public interface UserMapper {
      */
     @Select("select * from user where id = #{id}")
     User getById(Long id);
+
+    /**
+     *
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
 
